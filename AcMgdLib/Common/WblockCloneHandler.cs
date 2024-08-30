45cd4ccea1cbba621bb039099c74ab6452de1043
Tony <caddzooks@gmail.com>
2024-08-30 19:53:05 -0400

    Supplemental update

---
+++ AcMgdLib/Common/WblockCloneHandler.cs
@@ -0,0 +1,194 @@
+﻿/// WblockCloneHandler.cs
+/// 
+/// ActivistInvestor / Tony T
+/// 
+/// Distributed unter the terms of the MIT license
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Extensions;
+using System.Runtime.CompilerServices;
+using Autodesk.AutoCAD.ApplicationServices;
+
+/// This code requires C# 10.0
+
+namespace Autodesk.AutoCAD.DatabaseServices.Extensions
+{
+
+   /// <summary>
+   /// WblockCloneHandler class:
+   /// 
+   /// Does all the grunt work required to allow 
+   /// intervention in a wblock clone operation.
+   /// 
+   /// Usage requires one to derive a type from this 
+   /// class, that overrides the OnDeepCloneEnded()
+   /// virtual method, and within that override, do 
+   /// whatever work is needed.
+   /// 
+   /// See the WblockGroupHandler class for an example
+   /// showing the use of this type.
+   /// </summary>
+
+   public abstract class WblockCloneHandler
+   {
+      Database sourceDb;
+      Database destDb = null;
+      IdMapping idMap = null;
+
+      public WblockCloneHandler(Database db) 
+      { 
+         this.sourceDb = db;
+         db.WblockNotice += wblockNotice;
+      }
+
+      /// <summary>
+      /// Caveat emptor: The IdMapping is not usable
+      /// from a handler of the BeginSave event, and 
+      /// will terminate AutoCAD if accessed from that 
+      /// context. The IdMapping must not be accessed 
+      /// after the DeepCloneEnded event was raised.
+      /// </summary>
+
+      protected IdMapping IdMap
+      {
+         get
+         {
+            if(idMap == null || idMap.IsDisposed)
+               throw new InvalidOperationException("null or invalid IdMapping");
+            return idMap;
+         }
+      }
+
+      protected Database Source
+      {
+         get
+         {
+            if(sourceDb == null || sourceDb.IsDisposed)
+               throw new InvalidOperationException("source database is null or disposed");
+            return destDb;
+         }
+      }
+
+      protected Database Destination
+      {
+         get
+         {
+            if(destDb == null || destDb.IsDisposed)
+               throw new InvalidOperationException("destination database is null or disposed");
+            return destDb;
+         }
+      }
+
+      protected virtual void Reset()
+      {
+         this.idMap = null;
+         this.destDb = null;
+         sourceDb.WblockNotice += wblockNotice;
+      }
+
+      void wblockNotice(object sender, WblockNoticeEventArgs e)
+      {
+         Report();
+         Database.DatabaseConstructed += databaseConstructed;
+         // Added again in DeepCloneEnded/Aborted, to avoid reentry:
+         sourceDb.WblockNotice -= wblockNotice;
+      }
+
+      void databaseConstructed(object sender, EventArgs e)
+      {
+         Report();
+         Database.DatabaseConstructed -= databaseConstructed;
+         destDb = (Database)sender;
+         destDb.BeginDeepClone += beginDeepClone;
+      }
+
+      void beginDeepClone(object sender, IdMappingEventArgs e)
+      {
+         Report();
+         Database db = (Database)sender;
+         CheckSender(sender, destDb);
+         this.idMap = e.IdMapping;
+         db.BeginDeepClone -= beginDeepClone;
+         db.DeepCloneEnded += deepCloneEnded;
+         db.DeepCloneAborted += deepCloneAborted;
+         db.BeginDeepCloneTranslation += beginDeepCloneTranslation;
+      }
+
+      void beginDeepCloneTranslation(object sender, IdMappingEventArgs e)
+      {
+         Report();
+         Database db = (Database)sender;
+         CheckSender(sender, destDb);
+         db.BeginDeepCloneTranslation -= beginDeepCloneTranslation;
+         if(this.IdMap != e.IdMapping)
+            return;
+         OnBeginDeepCloneTranslation(e.IdMapping);
+      }
+
+      void deepCloneEnded(object sender, EventArgs e)
+      {
+         Report();
+         Database db = (Database)sender;
+         CheckSender(sender, destDb);
+         db.DeepCloneEnded -= deepCloneEnded;
+         OnDeepCloneEnded(db);
+         Reset();
+      }
+
+      void deepCloneAborted(object sender, EventArgs e)
+      {
+         Report();
+         Database db = (Database)sender;
+         CheckSender(sender, destDb);
+         db.DeepCloneAborted -= deepCloneAborted;
+         OnDeepCloneAborted(db);
+         Reset();
+      }
+
+      protected virtual void OnBeginDeepCloneTranslation(IdMapping map)
+      {
+      }
+
+      protected virtual void OnDeepCloneAborted(Database sender)
+      {
+      }
+
+      /// <summary>
+      /// This method must be overridden in a derived type
+      /// to perform whatever operations are required when
+      /// the wblock clone operation has ended.
+      /// </summary>
+      /// <param name="sender"></param>
+
+      protected abstract void OnDeepCloneEnded(Database sender);
+
+      [Conditional("DEBUG")]
+      protected static void Report([CallerMemberName] string msg = "(unknown)")
+      {
+         DebugWrite($"*** {msg} ***");
+      }
+
+      protected static void WriteMessage(string fmt, params object[] args)
+      {
+         var doc = Application.DocumentManager.MdiActiveDocument;
+         doc?.Editor.WriteMessage("\n" + fmt, args);
+      }
+
+      [Conditional("DEBUG")]
+      protected static void DebugWrite(string fmt, params object[] args)
+      {
+         var doc = Application.DocumentManager.MdiActiveDocument;
+         doc?.Editor.WriteMessage("\n" + fmt, args);
+      }
+
+      static void CheckSender(object sender, Database expected)
+      {
+         if((Database) sender != expected)
+            throw new InvalidOperationException("Wrong database");
+      }
+
+   }
+
+
+}
---
+++ AcMgdLib/Common/WblockGroupHandler.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.DatabaseServices.Extensions;
+using Autodesk.AutoCAD.Runtime;
+
+/// <summary>
+/// An example showing the use of the WblockCloneHandler
+/// base type, which automates the grunt work required to
+/// intervene in a wblock clone operation. 
+/// 
+/// This example causes groups to be included in a WBLOCK or a
+/// COPYCLIP operation when alll of their member entities are 
+/// included in the operation. 
+/// 
+/// To enable the functionality provided by this example, one 
+/// only need to issue the WBLOCKGROUPS command defined below. 
+/// 
+/// In a realistic usage scenario, the initialization done by
+/// the WBLOCKGROUPS command might instead by done within the
+/// Initialize() method of an IExtensionApplication.
+/// 
+/// Once WBLOCKGROUPS is issued, you can use the COPYCLIP and
+/// PASTECLIP commands to copy and paste entities, including 
+/// any groups they belong to.
+/// 
+/// In order for a group to be included in a WBLOCK operation,
+/// all of the member entities in the group must be included in
+/// the operation. If only a subset of member entities of a group
+/// are involved in a WBLOCK operation, the group(s) they belong 
+/// to are not included.
+/// 
+/// If COPYCLIP is used to copy entities and groups they are
+/// members of the clipboard, when pasted back into the same
+/// drawing or another drawing, the groups will be pasted as
+/// anonymous/unnamed groups. 
+/// 
+/// Because this operation does not technically Clone existing
+/// groups, if there is any type of application-data attached to 
+/// a group (e.g., xdata or extension dictionary) it will not be
+/// copied/cloned or transformed. Supporting that is beyond the
+/// scope of this example.
+/// </summary>
+
+namespace AcMgdLib.Common.Examples
+{
+   public class WblockGroupHandler : WblockCloneHandler
+   {
+      public WblockGroupHandler(Database db) : base(db)
+      {
+      }
+
+      protected override void OnDeepCloneEnded(Database sender)
+      {
+         CloneGroups(this.IdMap);
+      }
+
+      /// <summary>
+      /// Clones all groups from the source database
+      /// to the destination database, only if all of 
+      /// the entities in the group were cloned. 
+      /// </summary>
+      /// <param name="map"></param>
+
+      static void CloneGroups(IdMapping map)
+      {
+         if(map == null)
+            throw new ArgumentNullException(nameof(map));
+         try
+         {
+            using(var tr = new OpenCloseTransaction())
+            {
+               Database sourceDb = map.OriginalDatabase;
+               Database destDb = map.DestinationDatabase;
+               ObjectId destGroupDictionaryId = destDb.GroupDictionaryId;
+               var groupDictionary = tr.GetObject<DBDictionary>(
+                  destGroupDictionaryId, OpenMode.ForWrite);
+               foreach(var srcGroup in sourceDb.GetGroups(tr))
+               {
+                  if(srcGroup.IsNotAccessible)
+                     continue;
+                  var cloneIds = GetCloneIds(srcGroup, map);
+                  if(cloneIds != null)
+                  {
+                     Group group = new Group(srcGroup.Description, srcGroup.Selectable);
+                     groupDictionary.SetAt(srcGroup.Name, group);
+                     tr.AddNewlyCreatedDBObject(group, true);
+                     group.Append(cloneIds);
+                     DebugWrite($"Copied group {srcGroup.Name} ({cloneIds.Count} entities)");
+                  }
+               }
+               tr.Commit();
+            }
+         }
+         catch(System.Exception ex)
+         {
+            WriteMessage($"Exception in {nameof(CloneGroups)}(): {ex.ToString()}");
+            return;
+         }
+      }
+
+      /// <summary>
+      /// If not all source entities exist in the map (e.g., they
+      /// were not cloned), then this returns null and the group
+      /// is not cloned.
+      /// </summary>
+
+      public static ObjectIdCollection GetCloneIds(Group source, IdMapping map)
+      {
+         var srcIds = source.GetAllEntityIds();
+         var cloneIds = new ObjectId[srcIds.Length];
+         for(int i = 0; i < srcIds.Length; i++)
+         {
+            var id = srcIds[i];
+            if(!map.Contains(id))
+               return null;
+            cloneIds[i] = map[id].Value;
+         }
+         return new ObjectIdCollection(cloneIds);
+      }
+
+   }
+
+   public static class WBlockGroupExtensions
+   {
+      public static IEnumerable<Group> GetGroups(this Database db, Transaction tr)
+      {
+         var groupDict = (DBDictionary)tr.GetObject(db.GroupDictionaryId, OpenMode.ForRead);
+         foreach(DictionaryEntry entry in groupDict)
+         {
+            yield return Unsafe.As<Group>(tr.GetObject((ObjectId)entry.Value, OpenMode.ForRead));
+         }
+      }
+
+      public static T GetObject<T>(this Transaction tr, ObjectId id, OpenMode mode = OpenMode.ForRead)
+         where T: DBObject
+      {
+         return Unsafe.As<T>(tr.GetObject(id, mode));
+      }
+
+
+   }
+
+   public static class WblockGroupHandlers
+   {
+      static bool initialized = false;
+
+      public static void Initialize()
+      {
+         if(!initialized)
+         {
+            initialized = true;
+            foreach(Document doc in Application.DocumentManager)
+            {
+               doc.UserData[typeof(WblockGroupHandler)] =
+                  new WblockGroupHandler(doc.Database);
+            }
+
+            Application.DocumentManager.DocumentCreated += documentCreated;
+         }
+      }
+
+      private static void documentCreated(object sender, DocumentCollectionEventArgs e)
+      {
+         e.Document.UserData[typeof(WblockGroupHandler)] =
+            new WblockGroupHandler(e.Document.Database);
+      }
+   }
+
+   public static class TestCommand
+   {
+      [CommandMethod("WBLOCKGROUPS")]
+      public static void Initialize()
+      {
+         WblockGroupHandlers.Initialize();
+      }
+   }
+
+}
