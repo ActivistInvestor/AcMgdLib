0d75d06334416dd031f55c3af0e06dfaea2ab953
Tony <caddzooks@gmail.com>
2024-10-17 16:15:15 -0400

    Initial Commit

--- AcMgdLib/Overrules/Examples/ObservableDeepCloneExtensions.cs
+++ AcMgdLib/Overrules/Examples/ObservableDeepCloneExtensions.cs
@@ -10,8 +10,15 @@
 /// pair of source/clone objects, allowing either or both
 /// of same to be acted on within the deep clone operation.
 /// 
+/// Revisions:
+/// 
+/// Renamed all extension methods to "CopyObjects()" to
+/// reflect their (roughly) functional equivalence to the 
+/// ActiveX API's AcadDatabase.CopyObjects() method.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.EditorInput;
@@ -19,12 +26,14 @@ using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using AcRx = Autodesk.AutoCAD.Runtime;
 
+#pragma warning disable CS0618 // Type or member is obsolete
+
 namespace Autodesk.AutoCAD.DatabaseServices.Extensions
 {
    public static class ObservableDeepCloneExtensions
    {
       /// <summary>
-      /// An extension method that overloads the Database's
+      /// An extension method that complements the Database's
       /// DeepCloneObjects() method, adding support for operating
       /// on each source/clone pair at the point when each clone 
       /// is created and added to its owner, while it is open for
@@ -39,7 +48,8 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// using the stock DeepCloneObjects() method followed by a 
       /// post-processing loop that operates on all of the clones. 
       /// 
-      /// This is mainly because the delegate passed to this method 
+      /// The performance advantage of this method largely comes
+      /// from the fact that the delegate passed to this method 
       /// is called while the source and its clone are still open 
       /// within the deep-clone operation. 
       /// 
@@ -47,11 +57,11 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// delegate passed to this method can also operate on the 
       /// source object by upgrading it to OpenMode.ForWrite.
       /// 
-      /// Included below is a basic example, the MyCopyCommand() 
-      /// method. As can be seen in the example, after the entities
-      /// have been cloned, there's no need to start a transaction; 
-      /// iterate over an IdMapping and open each clone to transform 
-      /// it. The delegate passed to DeepCloneObjects<T>() does that, 
+      /// Included MyCopy() basic example method is included below.
+      /// As can be seen in the example, after the entities have been 
+      /// cloned, there's no need to start a transaction; iterate 
+      /// over an IdMapping and open each clone to transform it. 
+      /// The delegate used by the CopyObjects() method does that, 
       /// effectively-reducing the task of transforming the clones 
       /// to a single line of code.
       ///   
@@ -65,10 +75,11 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// <param name="ids">The ObjectIdCollection containing the 
       /// objects to be cloned. All objects must have the same owner.</param>
       /// <param name="ownerId">The ObjectId of the destination owner
-      /// object which the clones are to be added to.</param>
+      /// object which the clones are to be added to, or ObjectId.Null
+      /// to add the clones to the owner of the source objects.</param>
       /// <param name="primaryOnly">A value indicating if the action
-      /// should be invoked on all source/clone pairs, or only those
-      /// that are primary source/clone pairs.</param>
+      /// should be invoked on all source/clone pairs, or only primary 
+      /// source/clone pairs.</param>
       /// <param name="action">A delegate that takes two instances of
       /// the generic argument type, the first being the source object
       /// and the second being the clone of it. The source object is 
@@ -76,7 +87,7 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// <returns>An IdMapping instance representing the result of 
       /// the operation</returns>
 
-      public static IdMapping DeepCloneObjects<T>(this Database db, 
+      public static IdMapping CopyObjects<T>(this Database db, 
          ObjectIdCollection ids, 
          ObjectId ownerId,
          bool primaryOnly, 
@@ -84,40 +95,37 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       {
          if(db == null)
             throw new ArgumentNullException(nameof(db));
-         if(action == null)
-            throw new ArgumentNullException(nameof(action));
-         IdMapping map = new IdMapping();
-         using(new Overrule<T>(ownerId, primaryOnly, action))
-         {
-            db.DeepCloneObjects(ids, ownerId, map, false);
-         }
-         return map;
+         if(ids == null)
+            throw new ArgumentNullException(nameof(ids));
+         if(ids.Count == 0)
+            return new IdMapping();
+         if(ownerId.IsNull)
+            ownerId = GetOwnerId(ids[0]);
+         if(db != ids[0].Database || db != ownerId.Database)
+            throw new AcRx.Exception(AcRx.ErrorStatus.WrongDatabase);
+         return DeepClone(db, ids, ownerId, primaryOnly, action);
       }
 
       /// <summary>
-      /// An overloaded version of the above that deep clones 
-      /// entities to the same owner space:
+      /// An overloaded version of the above that deep 
+      /// clones entities to the same owner space. The
+      /// ObjectIdCollection argument must contain only
+      /// ObjectIds of entities or a derived type.
       /// </summary>
 
-      public static IdMapping DeepCloneObjects<T>(this Database db,
+      public static IdMapping CopyObjects<T>(this Database db,
          ObjectIdCollection ids,
-         Action<T, T> action) where T : DBObject
+         Action<T, T> action) where T : Entity
       {
+         AssertAreAllEntities(ids);
          if(db == null)
             throw new ArgumentNullException(nameof(db));
-         if(action == null)
-            throw new ArgumentNullException(nameof(action));
-         IdMapping map = new IdMapping();
          if(ids.Count == 0)
-            return map;
+            return new IdMapping();
          ObjectId ownerId = GetOwnerId(ids[0]);
          if(ownerId.Database != db)
             throw new AcRx.Exception(AcRx.ErrorStatus.WrongDatabase);
-         using(new Overrule<T>(ownerId, true, action))
-         {
-            db.DeepCloneObjects(ids, ownerId, map, false);
-         }
-         return map;
+         return DeepClone<T>(db, ids, ownerId, true, action);
       }
 
       /// <summary>
@@ -131,16 +139,19 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// <param name="xform">The Matrix3d describing the
       /// transformation to be applied to each cloned entity</param>
 
-      public static IdMapping Copy(this Database db,
+      public static IdMapping CopyObjects(this Database db,
          ObjectIdCollection ids,
          Matrix3d xform) 
       {
-         return DeepCloneObjects<Entity>(db, ids, (s, c) => c.TransformBy(xform));
+         Action<Entity, Entity> action = null;
+         if(!xform.IsEqualTo(Matrix3d.Identity))
+            action = (source, clone) => clone.TransformBy(xform);
+         return CopyObjects<Entity>(db, ids, action);
       }
 
 
       /// <summary>
-      /// A Copy() extension method that targets ObjectIdCollection.
+      /// An extension method that targets ObjectIdCollection.
       /// </summary>
       /// <typeparam name="T">The type of the DBObject that is
       /// passed to the action. Only instances of the this argument
@@ -149,7 +160,7 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// <param name="ids">The ObjectIdCollection containing the 
       /// objects to be cloned. All objects must have the same owner.</param>
       /// <param name="ownerId">The ObjectId of the destination owner
-      /// object which the clones are to be added to or ObjectId.Null
+      /// object which the clones are to be added to, or ObjectId.Null
       /// to clone the objects to their current owner.</param>
       /// <param name="primaryOnly">A value indicating if the action
       /// should be invoked on all source/clone pairs, or only those
@@ -163,30 +174,23 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// <exception cref="ArgumentNullException"></exception>
       /// <exception cref="AcRx.Exception"></exception>
 
-      public static IdMapping Copy<T>(this ObjectIdCollection ids,
+      public static IdMapping CopyObjects<T>(this ObjectIdCollection ids,
          ObjectId ownerId,
          bool primaryOnly,
          Action<T, T> action) where T : DBObject
       {
          if(ids == null)
             throw new ArgumentNullException(nameof(ids));
-         if(action == null)
-            throw new ArgumentNullException(nameof(action));
-         IdMapping map = new IdMapping();
          if(ids.Count == 0)
-            return map;
+            return new IdMapping();
          if(ownerId.IsNull)
             ownerId = GetOwnerId(ids[0]);
-         if(ownerId.Database != ids[0].Database)
-            throw new AcRx.Exception(AcRx.ErrorStatus.WrongDatabase);
          Database db = ownerId.Database;
          if(db == null)
             throw new AcRx.Exception(AcRx.ErrorStatus.NoDatabase);
-         using(new Overrule<T>(ownerId, primaryOnly, action))
-         {
-            db.DeepCloneObjects(ids, ownerId, map, false);
-         }
-         return map;
+         if(db != ids[0].Database)
+            throw new AcRx.Exception(AcRx.ErrorStatus.WrongDatabase);
+         return DeepClone<T>(db, ids, ownerId, primaryOnly, action);
       }
 
       /// <summary>
@@ -195,24 +199,85 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// space, and transforming the copies.
       /// </summary>
       /// <param name="ids">An ObjectIdCollection containing the
-      /// Ids of the entities to be copied. All elements must be 
+      /// ids of the entities to be copied. All elements must be 
       /// references to Entity or a derived type, and must have
       /// the same owner.</param>
       /// <param name="xform">The transformation matrix to be 
-      /// applied to the clones of the input entities</param>
+      /// applied to the clones of the input entities. If this
+      /// argument is the Identity matrix no transformation is
+      /// performed.</param>
       /// <returns>An IdMapping representing the result of the
       /// operation</returns>
 
-      public static IdMapping Copy(this ObjectIdCollection ids, Matrix3d xform)
+      public static IdMapping CopyObjects(this ObjectIdCollection ids, Matrix3d xform)
       {
-         return Copy<Entity>(ids, ObjectId.Null, true, 
-            (source, clone) => clone.TransformBy(xform));
+         AssertAreAllEntities(ids);
+         Action<Entity, Entity> action = null;
+
+         /// If the caller passed the Identity matrix, there's
+         /// no transformation, and no need for the Overrule,
+         /// which is not used if we pass a null action:
+
+         if(!xform.IsEqualTo(Matrix3d.Identity))
+            action = (source, clone) => clone.TransformBy(xform);
+         
+         return CopyObjects<Entity>(ids, ObjectId.Null, true, action);
+      }
+
+      /// <summary>
+      /// An overload of the above method that targets IEnumerable<ObjectId>.
+      /// </summary>
+      
+      public static IdMapping CopyObjects(this IEnumerable<ObjectId> ids, Matrix3d xform)
+      {
+         return CopyObjects(new ObjectIdCollection(
+            ids as ObjectId[] ?? ids.ToArray()), xform);
+      }
+
+      /// <summary>
+      /// Worker for all CopyObjects() overloads:
+      /// </summary>
+
+      static IdMapping DeepClone<T>(Database db, ObjectIdCollection ids, ObjectId ownerId, bool primaryOnly, Action<T, T> action) where T : DBObject
+      {
+         IdMapping map = new IdMapping();
+         Overrule<T> overrule = null;
+         if(action != null)
+            overrule = new Overrule<T>(ownerId, primaryOnly, action);
+         using(overrule)
+         {
+            db.DeepCloneObjects(ids, ownerId, map, false);
+         }
+         return map;
       }
 
       static ObjectId GetOwnerId(ObjectId id)
       {
-         using(var tr = new OpenCloseTransaction())
-            return tr.GetObject(id, OpenMode.ForRead).OwnerId;
+         using(DBObject obj = id.Open(OpenMode.ForRead))
+         {
+            return obj is Entity ent ? ent.BlockId : obj.OwnerId;
+         }
+      }
+
+      static readonly RXClass entityClass = RXObject.GetClass(typeof(Entity));
+
+      //static Cache<RXClass, bool> entityClasses
+      //   = new Cache<RXClass, bool>(c => c.IsDerivedFrom(entityClass));
+
+      static void AssertAreAllEntities(this ObjectIdCollection ids)
+      {
+         if(ids == null)
+            throw new ArgumentNullException(nameof(ids));
+         for(int i = 0; i < ids.Count; i++)
+         {
+            ObjectId id = ids[i];
+            if(id.IsNull)
+               throw new AcRx.Exception(AcRx.ErrorStatus.NullObjectId);
+            //if(!entityClasses[id.ObjectClass])
+            //   throw new AcRx.Exception(AcRx.ErrorStatus.NotAnEntity);
+            if(!id.ObjectClass.IsDerivedFrom(entityClass))
+               throw new AcRx.Exception(AcRx.ErrorStatus.NotAnEntity);
+         }
       }
 
       class Overrule<T> : ObjectOverrule where T : DBObject
@@ -275,11 +340,11 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
       /// 
       /// With the help of the included extension methods, the
       /// operation of cloning the selection and transforming 
-      /// the clones can be done with a single line of code.
+      /// the clones is done in a single line of code.
       /// </summary>
 
       [CommandMethod("MYCOPY")]
-      public static void MyCopyCommand()
+      public static void MyCopy()
       {
          Document doc = Application.DocumentManager.MdiActiveDocument;
          Database db = doc.Database;
@@ -290,7 +355,7 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
          var psr = ed.GetSelection();
          if(psr.Status != PromptStatus.OK)
             return;
-         var ppo = new PromptPointOptions("\nBase point: ");
+         var ppo = new PromptPointOptions("\nBasepoint: ");
          var ppr = ed.GetPoint(ppo);
          if(ppr.Status != PromptStatus.OK)
             return;
@@ -301,10 +366,8 @@ namespace Autodesk.AutoCAD.DatabaseServices.Extensions
          ppr = ed.GetPoint(ppo);
          if(ppr.Status != PromptStatus.OK)
             return;
-         var ids = new ObjectIdCollection(psr.Value.GetObjectIds());
-         Matrix3d xform = Matrix3d.Displacement(from.GetVectorTo(ppr.Value));
-         db.DeepCloneObjects<Entity>(ids, 
-            (source, clone) => clone.TransformBy(xform));
+         psr.Value.GetObjectIds().CopyObjects(
+            Matrix3d.Displacement(from.GetVectorTo(ppr.Value)));
       }
    }
 }
