bb9813b6fadfcba4bfffb67a85e6794cf3a11dd9
Tony T <caddzooks@gmail.com>
2025-04-14 14:00:48 -0400

    Initial Commit

--- AcMgdLib/Extensions/LongTransactionExtensions.cs
+++ AcMgdLib/Extensions/LongTransactionExtensions.cs
@@ -101,7 +101,8 @@ public static class LongTransactionExtensions
    /// Works like the Editor's GetSelection() method, except that
    /// if there is a current workset, the selection is constrained
    /// to objects in the workset. If there's no current workset, 
-   /// the result is the same result returned by GetSelection().
+   /// the behavior is intentical to the Editor's GetSelection()
+   /// method.
    /// </summary>
    /// <param name="ed"></param>
    /// <param name="pso"></param>
